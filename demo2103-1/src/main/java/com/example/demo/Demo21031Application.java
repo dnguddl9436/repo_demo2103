20e7e61e94d0c7455916079eae7b581017124d64
dnguddl9436 <dnguddl9696@gmail.com>
2021-05-20 11:12:31 +0900

    컴파일 에러 테스트합니다.

--- demo2103-1/src/main/java/com/example/demo/Demo21031Application.java
+++ demo2103-1/src/main/java/com/example/demo/Demo21031Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo21031Application {
-
+	//Test클래스
 	public static void main(String[] args) {
 		SpringApplication.run(Demo21031Application.class, args);
 	}
